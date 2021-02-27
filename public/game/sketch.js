324cae4f9126f628d8c4ca60fae96301eeb280bd
Harry morgan <harrymorgan@Harrys-MacBook.local>
2021-02-27 10:59:58 +0000

    Sockets work now - reworks score to ++ per block on field

--- public/game/sketch.js
+++ public/game/sketch.js
@@ -3,114 +3,110 @@
 let socket;
 
 let user_config = {
+  speed: 5,
 
-    speed: 5,
+  randomizer: 0.01,
+  randomVal: 1.25,
 
-    randomizer: 0.01,
-    randomVal: 1.25,
-     
-    bg_r: 235,
-    bg_g: 235,
-    bg_b: 224,    
-    user_r: 47,
-    user_g: 47,
-    user_b: 30
-}
+  bg_r: 235,
+  bg_g: 235,
+  bg_b: 224,
+  user_r: 47,
+  user_g: 47,
+  user_b: 30,
+};
 
 let user;
 
-let blocks = []
+let blocks = [];
 let blockCount = 1;
 
 let score;
+let score2;
 let speedi;
 
 function keyPressed() {
-    if (key == ' ' && user.y == height - user.r) {
-        user.jump();
-    }
+  if (key == " " && user.y == height - user.r) {
+    user.jump();
+  }
 }
 
 function setup() {
+  socket = io.connect("http://localhost:3000");
 
-    socket = io.connect('http://localhost:3000')
+  createCanvas(800, 800);
 
-    createCanvas(800, 800);
-
-    user = new User();
-    score = 0;
-    speedi = 3000;
+  user = new User();
+  score = 0;
+  speedi = 3000;
 }
 
 function draw() {
-    background(user_config.bg_r, user_config.bg_g, user_config.bg_b);
-    rectMode(CORNER)
-    user.show()
-    user.move()
-    textSize(30);
+  background(user_config.bg_r, user_config.bg_g, user_config.bg_b);
+  rectMode(CORNER);
+  user.show();
+  user.move();
+  textSize(30);
+  stroke(0);
+  strokeWeight(1);
+  fill(0);
+  text("Score: " + score, 10, 30);
+
+  socket.on("die", (data) => {
+    score2 = data.s
+  });
+
+  textSize(30);
+  stroke(0);
+  strokeWeight(1);
+  fill(0);
+  text("Score 2: " + score2, 10, 60);
+
+  if (random(user_config.randomVal) < user_config.randomizer) {
+    console.log("randomval: ", user_config.randomVal);
+    console.log("randomizer: ", user_config.randomizer);
+
+    var i;
+    for (i = 0; i <= blockCount; i++) {
+      blocks.push(new Block());
+      score++;
+      sendScore(score)
+      console.log("block push " + i);
+    }
+  }
+
+  //if (user && blocks.length >= 1) {
+  //}
+
+  for (let t of blocks) {
     stroke(0);
-    strokeWeight(1);
-    fill(0);
-    text('Score: ' + score, 10, 30)
+    strokeWeight(2);
+    t.show();
 
-    sendScore(score)
+    if (score > speedi) {
+      speedi += 2000;
+      user_config.speed += 2;
 
-    textSize(30);
-    stroke(0);
-    strokeWeight(1);
-    fill(0);
-    socket.on('die', data => {
-        text('Score 2: ' + data.s, 110, 30)
-        }
-    )
-    
-
-
-    if (random(user_config.randomVal) < user_config.randomizer) {
-        
-        console.log('randomval: ', user_config.randomVal)
-        console.log('randomizer: ', user_config.randomizer)
-
-        var i;
-        for (i = 0; i <= blockCount; i++) {
-            blocks.push(new Block());
-            console.log('block push ' + i)
-        }
+      if (user_config.randomizer < 0.015) {
+        user_config.randomizer += 0.005;
+        user_config.randomVal -= 0.05;
+        blockCount++;
+      }
+      console.log("speed: ", user_config.speed);
     }
 
-    if (user && blocks.length >= 1) {
-        score++;
+    if (user.hits(t)) {
+      strokeWeight(4);
+      fill(0);
+      textSize(50);
+      text("YOU DIED", 250, 200);
+      //noLoop();
     }
-
-    for (let t of blocks) {
-        stroke(0);
-        strokeWeight(2);
-        t.show();
-
-        if (score > speedi) {
-            speedi += 2000;
-            user_config.speed += 2;
-
-            if (user_config.randomizer < 0.015) {
-                user_config.randomizer += 0.005;
-                user_config.randomVal -= 0.05
-                blockCount++
-            }
-            console.log('speed: ', user_config.speed)
-        } 
-
-        if (user.hits(t)) {
-            strokeWeight(4);
-            fill(0);
-            textSize(50);
-            text('YOU DIED', 250, 200);
-            //noLoop();
-        }
-    }
+  }
 }
 
-function sendScore(sc){
-    const data = {s: sc}
+function sendScore(sc) {
+  const data = { s: sc };
 
-    socket.emit('die', data)
+  socket.emit("die", data);
 }
